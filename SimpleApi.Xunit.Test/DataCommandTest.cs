3a8b52e069a7804cfb4edee7fed356b8eeaf823d
Jesper Petersen <info@ordklar.dk>
2021-07-23 01:23:39 +0200

    Update DataCommandTest.cs

--- SimpleApi.Xunit.Test/DataCommandTest.cs
+++ SimpleApi.Xunit.Test/DataCommandTest.cs
@@ -8,10 +8,62 @@ namespace SimpleApi.Xunit.Test
     {
         private readonly MockCommand _mockCommand = new MockCommand();
 
-        [Fact]
-        public void GetDataIsNotNull()
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        public void GetDataNotNullById_Single(int id)
         {
-            var result = _mockCommand.GetCommandSingleById(1);
+            var result = _mockCommand.GetCommandSingleById(id);
+            Assert.NotNull(result);
+        }
+
+        [Theory]
+        [InlineData(0, "Jesper Petersen")]
+        [InlineData(1, "Freja Hansen")]
+        [InlineData(2, "Mette Willumsen")]
+        public void GetDataSameById_Single(int id, string name)
+        {
+            var result = _mockCommand.GetCommandSingleById(id);
+            Assert.NotSame(result, name);
+        }
+
+        [Theory]
+        [InlineData(0, "Jesper Petersen")]
+        [InlineData(1, "Freja Hansen")]
+        [InlineData(2, "Mette Willumsen")]
+        public void GetDataEqualById_Single(int id, string name)
+        {
+            var result = _mockCommand.GetCommandSingleById(id);
+            Assert.Equal(name, result.Name);
+        }
+
+        [Theory]
+        [InlineData(1, "Jesper Petersen")]
+        [InlineData(0, "Freja Hansen")]
+        [InlineData(3, "Mette Willumsen")]
+        public void GetDataNotEqualById_Single(int id, string name)
+        {
+            var result = _mockCommand.GetCommandSingleById(id);
+            Assert.NotEqual(name, result.Name);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(-2)]
+        [InlineData(-3)]
+        [InlineData(-4)]
+        public void GetDataIsNullMulitById_Single(int id)
+        {
+            var result = _mockCommand.GetCommandSingleById(id);
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void GetDataIsNull()
+        {
+            var result = _mockCommand.GetAll();
             Assert.NotNull(result);
         }
     }
